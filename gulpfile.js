7ba4436025796da5f71725130dd6c88ef9ad5c31
demoncowgirl <bethsalvatore2018@gmail.com>
2019-05-31 22:42:33 -0400

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var runSequence = require('run-sequence');
+
+// task to copy all htm/html files from app directory to dist directory
+gulp.task('copyhtml', function() {
+  return gulp.src('app/*.+(htm|html)')
+	.pipe(gulp.dest('dist/'))
+ });
+
+gulp.task('copyimages', function(){
+  return gulp.src('app/images/*.+(png|jpg|gif)')
+  .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('copycss', function() {
+	return gulp.src('app/css/*.css')
+		.pipe(gulp.dest('dist/'))
+});
+
+gulp.task('copyjs', function() {
+	return gulp.src('app/js/*.js')
+		.pipe(gulp.dest('dist/'))
+});
+
+// function to change sass code to css
+gulp.task('sass', function() {
+	return gulp.src('app/scss/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('dist/'))
+});
+
+gulp.task('watch', function() {
+	gulp.watch('app/*.+(htm|html)', gulp.series('copyhtml'));
+  gulp.watch('app/**/*.css', gulp.series('copycss'));
+  gulp.watch('app/**/*.js', gulp.series('copyjs'));
+  gulp.watch('app/**/*.scss', gulp.series('sass'));
+  gulp.watch('app/**/*.+(jpg|jpeg|png|gif)', gulp.series('copyimages'));
+
+});
+
+gulp.task('default', function(callback) {
+	runSequence(['sass', 'copyhtml', 'copycss', 'copyjs'], callback)
+});
